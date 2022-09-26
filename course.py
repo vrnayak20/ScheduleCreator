b49480b495d55e963c07568f131065b4be5a3221
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2022-09-25 23:32:52 -0500

    Add files via upload

---
+++ course.py
@@ -0,0 +1,13 @@
+class Course:
+    def __init__(self, id, name, credits, tags,subject,term,grades,prerequisites,corequisites,schools, gpa):
+        self.id = id
+        self.name = name
+        self.credits = credits
+        self.tags = tags
+        self.subject = subject
+        self.term = term
+        self.grades = grades
+        self.prerequisites = prerequisites
+        self.corequisites = corequisites
+        self.schools = schools
+        self.gpa = gpa

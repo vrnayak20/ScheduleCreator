89004bb1553e579e5fa8c5fc2b763252019d4fbf
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2022-09-25 23:36:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import reading.storage
+
+def main():
+    print(vars(reading.storage.courses[0]))
+# adding things to lists and if conditions to
+# check if certain indexes like KAP/GT/AP exist
+
+main()

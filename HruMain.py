dc07717fccdb2eb8f06e624be9ecfdd367376b71
Hrudith <69597791+HrudithL@users.noreply.github.com>
2022-10-16 14:51:22 -0500

    Update and rename HruMain to HruMain.py

---
+++ HruMain.py
@@ -0,0 +1,50 @@
+from typing import Dict
+
+from writer.writer import Writer
+
+
+if input('Should we scrape? ').__contains__('y' or 'Y'):
+    writer = Writer()
+    writer.get_data()
+    writer.write()
+
+# import reader and do the reading here
+
+# basic info
+school = input('What school do you go to? ')
+major = input('What do you want to major in? ')
+
+# interest in fields of study
+print('On a Scale of 1 - 10 describe the following:')
+interest = {}
+interest['CS'] = int(input('Interest in Computer Science? '))
+interest['Buisness'] = int(input('Interest in Computer Science? '))
+interest['Public Speaking/Writing'] = int(input('Interest in Public Speaking/Writing? '))
+interest['Engineering'] = int(input('Interest in Engineering? '))
+interest['Medical'] = int(input('Interest in the Medical Field? '))
+interest['History'] = int(input('Interest in History? '))
+
+# middle school
+print('On the following courses, state if taken in middle school: ')
+taken = {}
+lang = input('What foreign language? ') if input('A foreign language? ').__contains__('y' or 'Y') else ""
+taken['Language'] = lang
+math = 'Geometry' if input('Geometry? ').__contains__('y' or 'Y') else 'Algebra' if input('Algebra? ').__contains__('y' or 'Y') else ""
+taken['Math'] = math
+
+# other intersts
+fine_arts = input('What fine arts did you take in Middle school')
+interest['Fine Arts'] = int(input(f'Interest in {fine_arts}? '))
+interest['GPA'] = int(input('Interest in GPA? '))
+interest['Fun'] = int(input('Interest in taking fun courses? '))
+interest['History'] = int(input('Interest in History? '))
+interest['History'] = int(input('Interest in History? '))
+interest['5.0 over Nothing'] = int(input('On a Scale of 1 - 10 if you had the option to take a course that you '
+                                         'preferred (5.0) or not take any course at all (Not Counted), '
+                                         'how much would you want to take the course? '))
+
+interest['4.0 over Nothing'] = int(input('On a Scale of 1 - 10 if you had the option to take a course that you '
+                                         'preferred (4.0) or not take any course at all (Not Counted), how much '
+                                         'would you want to take the course still? '))
+interest['AP'] = int(input('On a Scale of 1 - 10 how much do you value AP courses (Harder but provide college credit)'))
+interest['Easy'] = int(input('On a Scale of 1 - 10 how much would want to take a course you do not like but is easy? '))

367a141d8e9e51be8e229960303c76863470bf18
Hrudith <69597791+HrudithL@users.noreply.github.com>
2022-10-23 12:15:39 -0500

    Add files via upload

---
+++ analyser.py
@@ -0,0 +1,14 @@
+from reading.storage import Storage
+
+
+class Schedule:
+    ninth = []
+    tenth = []
+    eleventh = []
+    twelfth = []
+    courses = []
+
+    def __init__(self, school, interest):
+        self.storage = Storage(school)
+
+

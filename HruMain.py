ed84cca84cbf803d35e5b45b463c1c0fc4635497
Hrudith <69597791+HrudithL@users.noreply.github.com>
2022-10-23 12:17:23 -0500

    Update HruMain.py

--- HruMain.py
+++ HruMain.py
@@ -1,5 +1,5 @@
 from typing import Dict
-
+from reading.storage import Storage
 from writer.writer import Writer
 
 
@@ -9,27 +9,24 @@ if input('Should we scrape? ').__contains__('y' or 'Y'):
     writer.write()
 
 # import reader and do the reading here
+storing = Storage()
 
 # basic info
 school = input('What school do you go to? ')
-major = input('What do you want to major in? ')
 
 # interest in fields of study
 print('On a Scale of 1 - 10 describe the following:')
-interest = {}
-interest['CS'] = int(input('Interest in Computer Science? '))
-interest['Buisness'] = int(input('Interest in Computer Science? '))
-interest['Public Speaking/Writing'] = int(input('Interest in Public Speaking/Writing? '))
-interest['Engineering'] = int(input('Interest in Engineering? '))
-interest['Medical'] = int(input('Interest in the Medical Field? '))
-interest['History'] = int(input('Interest in History? '))
+interest = {'CS': int(input('Interest in Computer Science? ')), 'Business': int(input('Interest in Business? ')),
+            'Public Speaking/Writing': int(input('Interest in Public Speaking/Writing? ')),
+            'Engineering': int(input('Interest in Engineering? ')),
+            'Medical': int(input('Interest in the Medical Field? ')), 'History': int(input('Interest in History? '))}
 
 # middle school
 print('On the following courses, state if taken in middle school: ')
 taken = {}
 lang = input('What foreign language? ') if input('A foreign language? ').__contains__('y' or 'Y') else ""
 taken['Language'] = lang
-math = 'Geometry' if input('Geometry? ').__contains__('y' or 'Y') else 'Algebra' if input('Algebra? ').__contains__('y' or 'Y') else ""
+math = 'Geometry' if input('Geometry? ').__contains__('y' or 'Y') else 'Algebra' if input('Algebra 1? ').__contains__('y' or 'Y') else ""
 taken['Math'] = math
 
 # other intersts
@@ -37,7 +34,6 @@ fine_arts = input('What fine arts did you take in Middle school')
 interest['Fine Arts'] = int(input(f'Interest in {fine_arts}? '))
 interest['GPA'] = int(input('Interest in GPA? '))
 interest['Fun'] = int(input('Interest in taking fun courses? '))
-interest['History'] = int(input('Interest in History? '))
 interest['History'] = int(input('Interest in History? '))
 interest['5.0 over Nothing'] = int(input('On a Scale of 1 - 10 if you had the option to take a course that you '
                                          'preferred (5.0) or not take any course at all (Not Counted), '

d6304267613f62e8ca2d3d53ca1f21295059a19d
Hrudith <69597791+HrudithL@users.noreply.github.com>
2022-10-23 12:16:24 -0500

    Update course.py

--- reading/course.py
+++ reading/course.py
@@ -1,5 +1,5 @@
 class Course:
-    def __init__(self, id, name, credits, tags,subject,term,grades,prerequisites,corequisites,schools, gpa):
+    def __init__(self, id, name, credits, tags,subject,term,grades,prerequisites,corequisites,schools, gpa, fun):
         self.id = id
         self.name = name
         self.credits = credits
@@ -11,3 +11,17 @@ class Course:
         self.corequisites = corequisites
         self.schools = schools
         self.gpa = gpa
+        self.fun = fun
+
+    def __str__(self):
+        return f'ID: {self.id}, ' \
+               f'Name: {self.name}, ' \
+               f'Subject: {self.subject}, ' \
+               f'Tags: {self.tags}, ' \
+               f'GPA: {self.gpa}, ' \
+               f'Credits: {self.credits}, ' \
+               f'Grades Offered In: {self.grades}, ' \
+               f'Pre-reqs: {self.prerequisites}'
+
+    def __eq__(self, other):
+        return self.name == other.name

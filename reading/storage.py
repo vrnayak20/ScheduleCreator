6cbf3d24d8c10492af414d0d98f53b681abf44f0
Hrudith <69597791+HrudithL@users.noreply.github.com>
2022-10-29 20:01:27 -0500

    Update storage.py

--- reading/storage.py
+++ reading/storage.py
@@ -11,17 +11,14 @@ class Storage:
             reader = csv.DictReader(file)
             for row in reader:
                 # checking if the course has already been added
-
-                name = row['name'][:-1]
-                # removing the course identifiers and tags from them because they are also shown in the tags
-                if 'KAP/GT ' in name:
-                    name.replace('KAP/GT ', '')
-                if 'KAP ' in name:
-                    name.replace('KAP ', '')
-                if 'AP ' in name:
-                    name.replace('AP ', '')
-                if 'KVS ' in name:
-                    name.replace('KVS ', '')
+                name: str = row['name']
+                if name[-1] == ('A' or 'B'):
+                    name = name[:-1]
+                # if 'A ' in name:
+                #    name.replace('A ', '')
+                ## removing the course identifiers and tags from them because they are also shown in the tags
+                if 'KVS' in name:
+                    continue
 
                 credit = float(row['credits'])
                 tags = row['tags'].split(',')
@@ -36,12 +33,24 @@ class Storage:
                     gpa = 4.5
                 course = Course(row['id'], name, credit, tags, row['subject'], row['term'], grades, prereqs, coreqs, schools, gpa, row['elective'])
                 valid = True
+
                 for val in self.courses:
-                    if course == val:
+                    if val == course:
                         valid = False
                         break
-                if valid and school in course.schools:
+
+                if valid and school in course.schools and 'KVS' not in tags:
                     self.courses.append(course)
-                # make sure corequisites also work as prerequisites when making schedule
-            for course in self.courses:
-                print(course)
+
+    def get_by_name(self, name, level='', grade=''):
+        for i in range(len(self.courses)):
+            if name in self.courses[i].name and (level == '' or level in self.courses[i].name) \
+                    and (grade == '' or grade in self.courses[i].grades):
+                return self.courses[i]
+        return None
+
+    def get_by_id(self, id):
+        for course in self.courses:
+            if course.id == id:
+                return course
+        return None
